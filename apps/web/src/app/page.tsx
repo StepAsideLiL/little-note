a9d0a061abd1dfa882fb975cb724b15ad61bf1d5
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-16 22:36:41 +0600

    feat(web):add icon.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,13 @@
 import Editor from "@/components/Editor";
 import SwitchThemeButton from "@/components/SwitchThemeButton";
-import { Button } from "@workspace/design-system/ui/button";
-import Link from "next/link";
 
 export default function Page() {
   return (
     <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-1 py-10">
       <h1 className="text-2xl font-bold">Hi! 👋 </h1>
-      <p>Welcome to Little Proj.</p>
 
       <div className="flex items-center gap-2">
-        <Button asChild>
-          <Link
-            href={"https://stepasidelil.vercel.app/projects/little-proj"}
-            target="_blank"
-          >
-            Read Docs and Start Coding
-          </Link>
-        </Button>
+        <p>Welcome to Little Note.</p>
         <SwitchThemeButton />
       </div>
 
