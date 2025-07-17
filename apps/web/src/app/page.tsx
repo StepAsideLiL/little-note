a5ad7a319778b679dd6dc6af62c1654a8ce5a184
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-17 10:28:49 +0600

    refactor(web): move Editor to editor dir and clean home page.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,8 @@
-import Editor from "@/components/Editor";
-import SwitchThemeButton from "@/components/SwitchThemeButton";
+import Editor from "@/components/editor/Editor";
 
 export default function Page() {
   return (
-    <main className="mx-auto flex w-full max-w-5xl flex-col items-center justify-center gap-1 py-10">
-      <h1 className="text-2xl font-bold">Hi! 👋 </h1>
-
-      <div className="flex items-center gap-2">
-        <p>Welcome to Little Note.</p>
-        <SwitchThemeButton />
-      </div>
-
+    <main className="mx-auto w-full max-w-3xl">
       <Editor />
     </main>
   );
