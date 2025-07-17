940083d77e79ea83bc5f5962115dfa0c1d2ca4c5
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-17 19:15:04 +0600

    feat(editor): add bold, italict and underline style. refactor(editor): pass all class name through cn function. fix(editor): markdown heading shortcut.

--- packages/editor/src/extension.ts
+++ packages/editor/src/extension.ts
@@ -1,30 +1,36 @@
 // import * as Blockquote from "@tiptap/extension-blockquote";
-// import * as Bold from "@tiptap/extension-bold";
+import Bold from "@tiptap/extension-bold";
 // import * as Color from "@tiptap/extension-color";
 import Document from "@tiptap/extension-document";
 import Heading from "@tiptap/extension-heading";
 // import * as Image from "@tiptap/extension-image";
-// import * as Italic from "@tiptap/extension-italic";
+import Italic from "@tiptap/extension-italic";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 // import * as TextStyle from "@tiptap/extension-text-style";
 // import * as TextAlign from "@tiptap/extension-text-align";
-// import * as Underline from "@tiptap/extension-underline";
+import Underline from "@tiptap/extension-underline";
 import { textblockTypeInputRule } from "@tiptap/react";
+import { cn } from "@workspace/design-system/lib/utils";
 
 export const extensions = [
+  Bold.configure({
+    HTMLAttributes: {
+      class: "font-bold",
+    },
+  }),
   Document,
   Heading.configure({
     levels: [2, 3, 4],
     HTMLAttributes: {
-      class: "heading",
+      class: cn("heading"),
     },
   }).extend({
     addInputRules() {
       return this.options.levels.map((level: number) => {
         return textblockTypeInputRule({
           find: new RegExp(
-            `^(#{${Math.min(...this.options.levels) - 1},${level}})\\s$`
+            `^(#{${Math.min(...this.options.levels) - 1},${level - 1}})\\s$`
           ),
           type: this.type,
           getAttributes: {
@@ -34,10 +40,20 @@ export const extensions = [
       });
     },
   }),
+  Italic.configure({
+    HTMLAttributes: {
+      class: cn("italic"),
+    },
+  }),
   Paragraph.configure({
     HTMLAttributes: {
-      class: "text-base font-normal leading-normal",
+      class: cn("text-base font-normal leading-normal"),
     },
   }),
   Text,
+  Underline.configure({
+    HTMLAttributes: {
+      class: cn("underline"),
+    },
+  }),
 ];
