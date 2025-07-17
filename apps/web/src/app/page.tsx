afd269fdb46a037d669856e7b0d2df1084449c6a
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-17 16:31:42 +0600

    feat(web): add heading.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import Editor from "@/components/editor/Editor";
 
 export default function Page() {
   return (
-    <main className="mx-auto w-full max-w-3xl">
+    <main className="mx-auto w-full max-w-3xl py-10">
       <Editor />
     </main>
   );
--- apps/web/src/components/editor/Editor.tsx
+++ apps/web/src/components/editor/Editor.tsx
@@ -1,15 +1,60 @@
 "use client";
 
-import { EditorContent, useEditor } from "@workspace/editor";
+import {
+  EditorContent,
+  textblockTypeInputRule,
+  useEditor,
+} from "@workspace/editor";
 import { extension } from "@workspace/editor/extension";
+import "@workspace/editor/editor.css";
 
 export default function Editor() {
   const editor = useEditor({
-    extensions: [extension.Document, extension.Paragraph, extension.Text],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      extension.Heading.configure({
+        levels: [2, 3, 4],
+        HTMLAttributes: {
+          class: "heading",
+        },
+      }).extend({
+        addInputRules() {
+          return this.options.levels.map((level: number) => {
+            return textblockTypeInputRule({
+              find: new RegExp(
+                `^(#{${Math.min(...this.options.levels) - 1},${level}})\\s$`
+              ),
+              type: this.type,
+              getAttributes: {
+                level,
+              },
+            });
+          });
+        },
+      }),
+      extension.Document,
+      extension.Paragraph.configure({
+        HTMLAttributes: {
+          class: "text-base font-normal leading-normal",
+        },
+      }),
+      extension.Text,
+    ],
+    content: `<h2>Hello World! 🌎️</h2>
+<h3>This is a heading 2.</h3>
+<h4>This is a heading 3.</h4>
+<p>This is a paragraph.</p>
+    `,
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
   });
 
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      <EditorContent editor={editor} />
+
+      <pre className="text-muted-foreground">
+        {JSON.stringify(editor?.getJSON(), null, 2)}
+      </pre>
+    </>
+  );
 }
