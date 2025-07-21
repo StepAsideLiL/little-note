aa2c36d61d028ff4b7e5a11f1cd2f39ac014c910
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-21 19:19:02 +0600

    feat(web): save note in indexedDB.

--- apps/web/src/components/note/SaveNoteButton.tsx
+++ apps/web/src/components/note/SaveNoteButton.tsx
@@ -2,8 +2,12 @@
 
 import { store } from "@/lib/store";
 import { Button } from "@workspace/design-system/ui/button";
+import { iDB } from "@workspace/db";
+import { useRouter } from "next/navigation";
+import { toast } from "@workspace/design-system/lib/toast";
 
 export default function SaveNoteButton() {
+  const router = useRouter();
   const { get: noteTitle } = store.useNoteTitle();
   const { get: noteContent } = store.useNoteContent();
 
@@ -11,8 +15,16 @@ export default function SaveNoteButton() {
     <Button
       variant={"secondary"}
       className="h-7 cursor-pointer"
-      onClick={() => {
-        console.log(noteTitle, noteContent);
+      onClick={async () => {
+        await iDB
+          .createNote(noteTitle, noteContent)
+          .then((noteSlug) => {
+            router.push(`/${noteSlug}`);
+          })
+          .catch((error) => {
+            toast.error("Error saving note");
+            console.error(error);
+          });
       }}
     >
       Save Note
