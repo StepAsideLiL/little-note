6c58c100cf9670a41a58dfc2177a9fc76ada4e38
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-07-27 11:14:43 +0600

    feat(web): if no note exist redirect to my-note

--- apps/web/src/components/note/EditorIndexedDB.tsx
+++ apps/web/src/components/note/EditorIndexedDB.tsx
@@ -5,48 +5,40 @@ import Editor from "./Editor";
 import NoteTitleHeading from "./NoteTitleHeading";
 import { useEffect } from "react";
 import { store } from "@/lib/store";
-import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { Skeleton } from "@workspace/design-system/ui/skeleton";
 
 export default function EditorIndexedDB({ noteId }: { noteId: string }) {
   const note = useLiveQuery(() => iDB.getNoteByNoteId(noteId));
-  const pathname = usePathname();
+  const router = useRouter();
   const { set: setTitle } = store.useNoteTitle();
   const { set: setContent } = store.useNoteContent();
 
   useEffect(() => {
-    if (pathname === "/") {
-      if (note) {
-        setTitle(note.title);
-        setContent(note.note);
-      } else {
-        setTitle("");
-        setContent({});
-      }
-    } else if (pathname !== "/") {
-      if (note) {
-        setTitle(note.title);
-        setContent(note.note);
-      }
+    if (!note) {
+      const timeout = setTimeout(() => {
+        router.replace("/my-note");
+      }, 300);
+
+      return () => clearTimeout(timeout);
     }
-  }, [note, pathname, setContent, setTitle]);
+
+    if (note) {
+      setTitle(note.title);
+      setContent(note.note);
+    } else {
+      setTitle("");
+      setContent({});
+    }
+  }, [note, router, setContent, setTitle]);
 
   if (!note) {
-    if (pathname === "/") {
-      return (
-        <>
-          <NoteTitleHeading />
-          <Editor />
-        </>
-      );
-    } else {
-      return (
-        <div className="space-y-3">
-          <Skeleton className="h-10 w-full"></Skeleton>
-          <Skeleton className="h-6 w-full"></Skeleton>
-        </div>
-      );
-    }
+    return (
+      <div className="space-y-3">
+        <Skeleton className="h-10 w-full"></Skeleton>
+        <Skeleton className="h-6 w-full"></Skeleton>
+      </div>
+    );
   }
 
   return (
