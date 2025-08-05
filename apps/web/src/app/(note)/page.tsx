f04ca315f56cdaf42a88cc5ed558ee20290731ef
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-08-05 10:06:31 +0600

    chore(web): remove commented codes.

--- apps/web/src/app/(note)/page.tsx
+++ apps/web/src/app/(note)/page.tsx
@@ -1,17 +1,13 @@
-// import Editor from "@/components/note/Editor";
 import ActiveNoteId from "@/components/ActiveNoteId";
-// import EditorIndexedDB from "@/components/note/EditorIndexedDB";
-// import NoteTitleHeading from "@/components/note/NoteTitleHeading";
 import nextMetadata from "@/lib/next-metadata";
 import { Metadata } from "next";
 
-export const metadata: Metadata = nextMetadata("New Note", "Write your note");
+export const metadata: Metadata = nextMetadata();
 
 export default function Page() {
   return (
     <>
       <ActiveNoteId />
-      {/* <EditorIndexedDB noteId="draftNote" /> */}
     </>
   );
 }
