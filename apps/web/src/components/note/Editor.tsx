96930caf67becb88fe87f3a9815bd20a2e8112e6
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2025-08-05 14:25:10 +0600

    refactor(web): use EditorComponent for editor.

--- apps/web/src/components/note/Editor.tsx
+++ apps/web/src/components/note/Editor.tsx
@@ -1,34 +1,28 @@
 "use client";
 
-import { EditorContent, useEditor } from "@workspace/editor";
-import { extensions } from "@workspace/editor/extension";
 import "@workspace/editor/editor.css";
 import { store } from "@/lib/store";
 import { usePathname } from "next/navigation";
 import { iDB, TLittleNote } from "@workspace/db";
+import { Editor as EditorComponent } from "@workspace/editor";
 
 export default function Editor({ note }: { note?: TLittleNote }) {
   const { set } = store.useNoteContent();
   const { get: noteTitle } = store.useNoteTitle();
   const pathname = usePathname();
 
-  const editor = useEditor({
-    extensions: [...extensions],
-    content: note?.note ? note.note : "",
-    // Don't render immediately on the server to avoid SSR issues
-    immediatelyRender: false,
-    onUpdate: async ({ editor }) => {
-      if (pathname !== "/" && note) {
-        await iDB.updateNote(note.id, noteTitle, editor.getJSON());
-      } else {
-        set(editor.getJSON());
-      }
-    },
-  });
-
   return (
     <>
-      <EditorContent editor={editor} className="h-full" />
+      <EditorComponent
+        content={note?.note}
+        onContentUpdate={async (content) => {
+          if (pathname !== "/" && note) {
+            await iDB.updateNote(note.id, noteTitle, content);
+          } else {
+            set(content);
+          }
+        }}
+      />
     </>
   );
 }
